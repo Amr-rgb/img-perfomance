6b93aa8caa5cf214df168872122444b151d5a1fc
Amr Taha <amrshiha2@gmail.com>
2022-09-27 17:08:53 +0200

    adding hero section design

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import "./App.css";
+import { Hero } from "./components/Hero";
 
 function App() {
   return (
-    <div className="App">
-      <h1>hello world!</h1>
+    <div className="App text-white">
+      <Hero />
     </div>
   );
 }
